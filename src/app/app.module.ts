5d3110166d85749f544de5fd36eb1b038f8c3390
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2023-06-18 19:44:57 -0300

    Adicionando o preloading para a animação exibir mais rápido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RIVE_FOLDER, RiveModule } from 'ng-rive';
+import { RIVE_FOLDER, RIVE_VERSION, RIVE_WASM, RiveModule } from 'ng-rive';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -27,7 +27,15 @@ import { NgxHowlerService } from 'ngx-howler';
       provide: RIVE_FOLDER,
       useValue: 'assets/animations',
     },
+    {
+      provide: RIVE_WASM,
+      useValue: 'assets/rive/rive.wasm',
+    },
+    {
+      provide: RIVE_VERSION,
+      useValue: 'latest',
+    }
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
